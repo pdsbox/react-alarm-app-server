5557568d7e366593f5debc6362586805f8120300
pdsbox <blutiger009@gmail.com>
2023-02-03 13:16:33 +0900

    cors test

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const server = jsonServer.create()
 const router = jsonServer.router('data.json')
 const middlewares = jsonServer.defaults()
 
+// const http = require('http');
+
+// const PORT = process.env.PORT || 3000;
+// const httpServer = http.createServer((req, res) => {
+//     res.setHeader('Access-Control-Allow-origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('ok');
+// })
+
+
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
@@ -12,7 +25,7 @@ server.use(jsonServer.rewriter({
 }))
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+    console.log(`JSON Server is running`)
 })
 
 // Export the Server API
