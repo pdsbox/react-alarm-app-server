fc557b5ebfadcacc3cccbc59b86ebc79f8d08512
pdsbox <blutiger009@gmail.com>
2023-02-21 18:48:15 +0900

    dir

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 
-fs.copyFile('data.json', os.tmpdir() + '/data.json', function (err) {
+fs.copyFile('data.json', os.tmpdir() + 'data.json', function (err) {
     if (err) console.log(err);
     else console.log("copy file succeed to" + os.tmpdir());
 });
 const server = jsonServer.create()
-const router = jsonServer.router(path.resolve(os.tmpdir() + '/data.json'));
+const router = jsonServer.router(path.resolve(os.tmpdir() + 'data.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares)
